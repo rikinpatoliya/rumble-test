84b0313536203e4d40baab2cedc7df000f9da544
angelaheelydc <64112944+angelaheelydc@users.noreply.github.com>
2024-09-19 13:05:28 -0400

    Update

--- domain/src/main/java/com/rumble/domain/landing/usecases/ShouldSuggestNewAppVersionUseCase.kt
+++ domain/src/main/java/com/rumble/domain/landing/usecases/ShouldSuggestNewAppVersionUseCase.kt
@@ -2,6 +2,12 @@ package com.rumble.domain.landing.usecases
 
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.rumble.domain.analytics.domain.usecases.UnhandledErrorUseCase
+import com.rumble.domain.settings.domain.usecase.IsCurrentTimeStampOverTriggerUseCase
+import com.rumble.domain.settings.model.UserPreferenceManager
+import com.rumble.utils.RumbleConstants.SUGGESTED_APP_VERSION_DISPLAY_DELAY_MINUTES
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 private const val TAG = "ShouldSuggestNewAppVersionUseCase"
@@ -9,6 +15,8 @@ private const val TAG = "ShouldSuggestNewAppVersionUseCase"
 class ShouldSuggestNewAppVersionUseCase @Inject constructor(
     private val unhandledErrorUseCase: UnhandledErrorUseCase,
     private val isVersionNameGreaterUseCase: IsVersionNameGreaterUseCase,
+    private val userPreferenceManager: UserPreferenceManager,
+    private val isCurrentTimeStampOverTriggerUseCase: IsCurrentTimeStampOverTriggerUseCase
 ) {
 
     operator fun invoke(versionName: String): Boolean {
@@ -16,6 +24,12 @@ class ShouldSuggestNewAppVersionUseCase @Inject constructor(
             val suggestedVersion =
                 FirebaseRemoteConfig.getInstance().getString("suggested_app_version")
             isVersionNameGreaterUseCase(suggestedVersion, versionName)
+                    && isCurrentTimeStampOverTriggerUseCase(
+                lastTimeStamp = runBlocking { userPreferenceManager.lastNewVersionDisplayTimeStampFlow.first() },
+                triggerMillis = TimeUnit.MINUTES.toMillis(
+                    SUGGESTED_APP_VERSION_DISPLAY_DELAY_MINUTES
+                )
+            )
         } catch (e: Exception) {
             unhandledErrorUseCase(TAG, e)
             false
